1cab8fcf481c158202b5cba97568fe934299fb46
Harrison <harrison3000@users.noreply.github.com>
2022-11-20 11:25:53 -0300

    hastly made addstring

--- filter.go
+++ filter.go
@@ -1,6 +1,7 @@
 package bloomzinho
 
 import (
+	"encoding/binary"
 	"hash"
 	"hash/fnv"
 )
@@ -23,9 +24,48 @@ func NewFilter(bits, hashes int) *Filter {
 }
 
 func (f *Filter) AddString(s string) {
+	h := f.doHashString(s)
 
+	for _, v := range h {
+		f.set(v)
+	}
 }
 
 func (f *Filter) LookupString(s string) bool {
 	return false
+}
+
+func (f *Filter) doHashString(s string) []int {
+	f.h.Reset()
+	f.h.Write([]byte(s))
+	hash := f.h.Sum(nil)
+
+	nbits := len(f.state) * 8
+
+	if nbits > 65000 || f.nh > 8 {
+		panic("we dont do that here (yet)")
+	}
+
+	var ret []int
+
+	for i := 0; i < f.nh; i++ {
+		idx := i * 2
+
+		h := int(binary.LittleEndian.Uint16(hash[idx:]))
+
+		h %= nbits
+
+		ret = append(ret, h)
+	}
+
+	return ret
+}
+
+func (f *Filter) set(i int) {
+	bucket := i / 8
+	shift := i % 8
+
+	mask := uint8(1 << shift)
+
+	f.state[bucket] |= mask
 }
